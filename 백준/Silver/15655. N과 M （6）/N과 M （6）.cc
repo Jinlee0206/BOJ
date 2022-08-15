22fd918d663137dbbbca0c6c864bdce8865a3c26
Jinlee0206 <105345909+Jinlee0206@users.noreply.github.com>
2022-08-15 17:40:29 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+vector<int> vec;
+vector<int> res;
+
+void Print()
+{
+	for (int i = 0; i < m; i++)
+	{
+		cout << res[i] << " ";
+	}
+	cout << "\n";
+	return;
+}
+
+void DFS(int lv, int idx)
+{
+	if (lv == m)
+	{
+		Print();
+		return;
+	}
+	else
+	{
+		for (int i = idx; i < n; i++)
+		{
+			res.push_back(vec[i]);
+			DFS(lv + 1, i + 1);
+			res.pop_back();
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+	{
+		int a;
+		cin >> a;
+		vec.push_back(a);
+	}
+
+	sort(vec.begin(), vec.end());
+
+	DFS(0, 0);
+
+	return 0;
+}
