4c199f9aa0ce0bf5cbb045a790f3c05cd0e8684d
Jinlee0206 <105345909+Jinlee0206@users.noreply.github.com>
2022-10-19 17:45:43 +0900

    [Silver I] Title: 컴백홈, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1189. 컴백홈/컴백홈.cc
+++ 백준/Silver/1189. 컴백홈/컴백홈.cc
@@ -1,43 +1,44 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
+#include <cmath>
 
 using namespace std;
-
-int r, c, k, sy, sx, ey, ex;
+int const _min = 2147000000;
 
 const int dy[4] = { -1, 0, 1, 0 };
-const int dx[4] = { 0, -1, 0 ,1 };
+const int dx[4] = { 0, -1, 0, 1 };
 
-int visited[10][10];
+int n, m, k, sy, sx, ey, ex;
+
 char m_map[10][10];
+int visited[10][10];
 
-int DFS(int y, int x)
+int dfs(int y, int x)
 {
-	// 기저 사례 (탈출 조건) 6개짜리 길이 가짓수 ++
 	if (y == ey && x == ex)
 	{
 		if (k == visited[y][x]) return 1;
 		else return 0;
 	}
 
-	int res = 0;
-	for (int i = 0; i < 4; i++)
-	{
+	int cnt = 0;
+	
+	for (int i = 0; i < 4; i++) {
 		int ny = y + dy[i];
 		int nx = x + dx[i];
 
-		if (ny < 0 || nx < 0 || ny >= r || nx >= c || visited[ny][nx] || m_map[ny][nx] == 'T') continue;
-		// 최단 거리 찾기
-		visited[ny][nx] = visited[y][x] + 1;
-		
-		// 가짓수 총합 더하기
-		res += DFS(ny, nx);
+		if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx]) continue;
 
-		// visited 초기화
+		if (m_map[ny][nx] == 'T') continue;
+
+		visited[ny][nx] = visited[y][x] + 1;
+		cnt += dfs(ny, nx);
 		visited[ny][nx] = 0;
-	}
-	return res;
+	} 
+
+	return cnt;
 }
 
 int main()
@@ -45,23 +46,26 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	cin >> r >> c >> k;
+	cin >> n >> m >> k;
 
-	string str;
-	for (int i = 0; i < r; i++)
+	for (int i = 0; i < n; i++)
 	{
+		string str;
 		cin >> str;
-		for (int j = 0; j < c; j++)
+		for (int j = 0; j < str.size(); j++)
 		{
 			m_map[i][j] = str[j];
 		}
 	}
 
-	sy = r - 1, sx = 0;
-	ey = 0, ex = c - 1;
+	sy = n - 1;
+	sx = 0;
+	ey = 0;
+	ex = m - 1;
 
 	visited[sy][sx] = 1;
-	cout << DFS(sy, sx) << "\n";
+
+	cout << dfs(sy, sx) << "\n";
 
 	return 0;
 }
