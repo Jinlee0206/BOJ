4f7f2479415bed13bf5804315f71d1531cafeb43
Jinlee0206 <105345909+Jinlee0206@users.noreply.github.com>
2022-10-31 17:04:02 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -7,13 +7,15 @@ using namespace std;
 long long solution(long long n) {
     long long answer = -1;
     
-    long long a = sqrt(n);
-    
-    // n의 제곱근이 자연수가 아닌 경우에는 n/sqrt(n) = n 값이 자연수로 다시 저장되지 않는다
-    if(a*a == n)
+    for(long long i = 1; i * i <= n; i++)
     {
-        answer = pow(a+1, 2);
+        if((i*i) == n) 
+        {
+            answer = (i+1) * (i+1);
+            break;
+        }
     }
+    
     
     return answer;
 }
