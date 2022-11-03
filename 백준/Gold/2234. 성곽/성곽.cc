3c69ca6e7251d526b25c24f9c46e37c525d57c2f
Jinlee0206 <105345909+Jinlee0206@users.noreply.github.com>
2022-11-03 19:51:42 +0900

    [Gold III] Title: 성곽, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

---
+++ 백준/Gold/2234. 성곽/성곽.cc
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+const int dy[4] = { 0, -1, 0, 1 };
+const int dx[4] = { -1, 0, 1, 0 };
+
+int m, n, a[54][54], visited[54][54], cnt, res, compSize[2504], big, mx;
+
+int dfs(int y, int x, int cnt)
+{
+	if (visited[y][x]) return 0;
+	visited[y][x] = cnt;
+	int res = 1;
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (!(a[y][x] & (1 << i)))
+		{
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			res += dfs(ny, nx, cnt);
+		}
+	}
+	return res;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> m >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin >> a[i][j];
+		}
+	}
+
+	// 구역 설정 DFS
+	// mx 값 구하기
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (!visited[i][j])
+			{
+				cnt++;
+				compSize[cnt] = dfs(i, j, cnt);
+				mx = max(mx, compSize[cnt]);
+			}
+		}
+	}
+
+	// 완전탐색 설정
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (i + 1 < n)
+			{
+				int a = visited[i + 1][j];
+				int b = visited[i][j];
+				if (a != b)
+				{
+					big = max(big, compSize[a] + compSize[b]);
+				}
+			}
+			if (j + 1 < m)
+			{
+				int a = visited[i][j + 1];
+				int b = visited[i][j];
+				if (a != b)
+				{
+					big = max(big, compSize[a] + compSize[b]);
+				}
+			}
+		}
+	}
+
+	cout << cnt << "\n" << mx << "\n" << big << "\n";
+	
+	return 0;
+}
