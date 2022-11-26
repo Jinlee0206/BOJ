ad9e6f72defe9b542b600817717d63921e517954
Jinlee0206 <105345909+Jinlee0206@users.noreply.github.com>
2022-11-26 22:46:29 +0900

    [Gold II] Title: 보석 도둑, Time: 164 ms, Memory: 18532 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -1,51 +1,53 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <queue>
 #include <algorithm>
+#include <queue>
 
 typedef long long ll;
 using namespace std;
 
-int n, k;
-ll res = 0;
+int n, k, m, v, c;
+ll res;
+vector<pair<ll, ll>> vec;
+vector<ll> vec2;
+priority_queue<ll> pq;
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
-    cin >> n >> k;
+	cin >> n >> k;
 
-    vector<pair<ll, ll>> v(n);
-    vector<ll> vv(k);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> m >> v;
+		vec.push_back({ m,v });
+	}
 
-    for (int i = 0; i < n; i++)
-    {
-        cin >> v[i].first >> v[i].second;
-    }
+	for (int i = 0; i < k; i++)
+	{
+		int a; 
+		cin >> a;
+		vec2.push_back(a);
+	}
 
-    for (int i = 0; i < k; i++) cin >> vv[i];
+	sort(vec.begin(), vec.end());
+	sort(vec2.begin(), vec2.end());
 
-    // 가장 적은 무게의 보석들로 정렬
-    sort(v.begin(), v.end());
-    // 가장 적은 무게를 담을 수 있는 가방을 기준으로 정렬
-    sort(vv.begin(), vv.end());
+	int j = 0;
+	for (int i = 0; i < k; i++)
+	{
+		while (j < n && vec[j].first <= vec2[i]) pq.push(vec[j++].second);
 
-    priority_queue <ll> pq;
+		if (pq.size())
+		{
+			res += pq.top();
+			pq.pop();
+		}
+	}
 
-    int j = 0;
-    for (int i = 0; i < k; i++)
-    {
-        while (j < n && v[j].first <= vv[i]) pq.push(v[j++].second);
-        if (pq.size())
-        {
-            res += pq.top();
-            pq.pop();
-        }
-    }
+	cout << res << '\n';
 
-    cout << res << '\n';
-
-    return 0;
+	return 0;
 }
