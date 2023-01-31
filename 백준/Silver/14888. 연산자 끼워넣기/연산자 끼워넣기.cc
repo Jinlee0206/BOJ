2793b9b033f59eb3911afe1747b2fb7fe764058a
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2023-01-31 15:10:35 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -1,64 +1,43 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-int n;
-int m_min = 2147000000, m_max= -2147000000;
-vector<int> operands;
-vector<int> operators;
+int n, a[14], oper[4], m_min = 2147000000, m_max= -2147000000;
 
-void DFS(int res, int lv)
+void dfs(int res, int lv)
 {
 	if (lv == n)
 	{
-		m_min = min(m_min, res);
 		m_max = max(m_max, res);
-		return;
-	}
-	else
-	{
-		for (int i = 0; i < 4; i++)
-		{
-			if (operators[i] > 0)
-			{
-				operators[i]--;
-				if (i == 0)
-					DFS(res + operands[lv], lv + 1);
-				else if (i == 1)
-					DFS(res - operands[lv], lv + 1);
-				else if (i == 2)
-					DFS(res * operands[lv], lv + 1);
-				else
-					DFS(res / operands[lv], lv + 1);
-				operators[i]++;
-			}
-		}
-		return;
-	}
-}
-
-int main()
-{
-	cin >> n;
-
-	for (int i = 0; i < n; i++)
-	{
-		int a;
-		cin >> a;
-		operands.push_back(a);
+		m_min = min(m_min, res);
 	}
 
 	for (int i = 0; i < 4; i++)
 	{
-		int a;
-		cin >> a;
-		operators.push_back(a);
+		if (oper[i] > 0)
+		{
+			oper[i]--;
+			if (i == 0) dfs(res + a[lv], lv + 1);
+			else if (i == 1) dfs(res - a[lv], lv + 1);
+			else if (i == 2) dfs(res * a[lv], lv + 1);
+			else dfs(res / a[lv], lv + 1);
+			oper[i]++;
+		}
 	}
+}
 
-	DFS(operands[0], 1);
-	cout << m_max << " " << m_min << "\n";
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> a[i];
+	for (int i = 0; i < 4; i++) cin >> oper[i];
+
+	dfs(a[0], 1);
+
+	cout << m_max << '\n' << m_min << '\n';
+
+	
 	return 0;
 }
