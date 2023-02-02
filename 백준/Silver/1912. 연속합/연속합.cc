9bfa9909db0fdbb1bc08fc42d77f6f245bf9d7ed
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2023-02-02 17:18:37 +0900

    [Silver II] Title: 연속합, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -1,41 +1,29 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-int main()
-{
+const int INF = 2147000000;
+int n, a, sum, m_max = -INF;
+
+int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int n, res = 0;
 	cin >> n;
-
-	vector<int> vec;
-	vector<int> dp(n);
-
 	for (int i = 0; i < n; i++)
 	{
-		int a;
 		cin >> a;
-		vec.push_back(a);
+		sum += a;
+		m_max = max(m_max, sum);
+
+		if (sum < 0)
+		{
+			sum = 0;
+			continue;
+		}
 	}
 
-	res = vec[0];
-
-	for (int i = 0; i < n; i++)
-	{
-		dp[i] = vec[i];
-
-		if (i == 0) continue;
-		
-		if (dp[i] < dp[i - 1] + vec[i]) dp[i] = dp[i - 1] + vec[i];
-
-		if (dp[i] > res) res = dp[i];
-	}
-
-	cout << res << "\n";
-
+	cout << m_max;
+	
 	return 0;
 }
