d6f1d0c42a84da2acd00242f501e87005f05e89d
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2023-06-09 23:39:31 +0900

    [Silver IV] Title: 주몽, Time: 0 ms, Memory: 2080 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -1,36 +1,41 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+int a[15004];
+
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	int n, m, cnt =0;
-	cin >> n >> m;
+    int n, m, cnt = 0;
+    cin >> n >> m;
 
-	vector<int> vec(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
 
-	for (int i = 0; i < n; i++)
-	{
-		cin >> vec[i];
-	}
+    sort(a, a + n);
 
-	sort(vec.begin(), vec.end(), greater<>());
+    int p1, p2;
+    p1 = 0;
+    p2 = n-1;
 
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = i+1; j < n; j++)
-		{
-			if (vec[i] + vec[j] == m) cnt++;
-		}
-	}
+    while (p1 < p2)
+    {
+        if (a[p1] + a[p2] > m) p2--;
+        else if (a[p1] + a[p2] == m)
+        {
+            cnt++;
+            p1++;
+            p2--;
+        }
+        else p1++;
+    }
 
-	cout << cnt << "\n";
+    cout << cnt << '\n';
 
-	return 0;
+    return 0;
 }
