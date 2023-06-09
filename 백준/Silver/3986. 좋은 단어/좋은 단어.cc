1514c578838e7b70435ebbd2d296b4de3aa97ef8
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2023-06-10 00:11:18 +0900

    [Silver IV] Title: 좋은 단어, Time: 4 ms, Memory: 2488 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.cc
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -1,40 +1,29 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <stack>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	int n, cnt = 0;
-	cin >> n;
+    int n, cnt = 0;
+    cin >> n;
 
-	for (int i = 0; i < n; i++)
-	{
-		string str;
-		cin >> str;
-		
-		stack<char> stk;
+    for (int i = 0; i < n; i++)
+    {
+        stack<char> stk;
+        string str;
+        cin >> str;
 
-		for (int j = 0; j < str.length(); j++)
-		{
-			if (!stk.empty() && stk.top() == str[j])
-			{
-				stk.pop();
-				continue;
-			}
-			stk.push(str[j]);
-		}
+        for (int j = 0; j < str.size(); j++)
+        {
+            if (!stk.empty() && stk.top() == str[j]) stk.pop();
+            else stk.push(str[j]);
+        }
+        if (stk.empty()) cnt++;
+    }
+    cout << cnt << '\n';
 
-		if (stk.empty()) cnt++;
-	}
-
-	cout << cnt << "\n";
-
-	return 0;
+    return 0;
 }
