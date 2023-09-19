40d54e3586afe5907d4c5fd150968adcbb2b24aa
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2023-09-19 18:15:12 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,85 +1,65 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+// DFS를 활용한 Composite Component
 
+#include <bits/stdc++.h>
 using namespace std;
 
-int t, n, m, k, cnt = 0;
+const int dy[] = { -1, 0, 1, 0 };
+const int dx[] = { 0, 1, 0, -1 };
 
-int m_map[51][51];
-int visited[51][51];
+int T, n, m, k, x, y;
+int a[54][54], visited[54][54];
 
-const int dy[4] = { -1, 0, 1, 0 };
-const int dx[4] = { 0, -1, 0, 1 };
-
-// conected component 찾는 문제
-// DFS를 통해 visited 행렬 찾고 counting
-void DFS(int y, int x)
+void go(int y, int x)
 {
-	visited[y][x] = 1;
-	for (int i = 0; i < 4; i++)
-	{
-		int ny = y + dy[i];
-		int nx = x + dx[i];
+    visited[y][x] = 1; // 시작지점 방문처리 미리 (돌다리를 두들겨보기)
 
-		if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-		
-		if (m_map[ny][nx] == 1 && !visited[ny][nx])
-			DFS(ny, nx);
-	}
-	return;
+    for (int i = 0; i < 4; i++)
+    {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+
+        if (ny < 0 || nx < 0 || ny >= n || nx >= m || !a[ny][nx]) continue;
+        if (visited[ny][nx]) continue;
+        go(ny, nx);
+    }
+    return;
 }
 
 int main()
-{	
-	cin >> t;
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	while (t--)
-	{
-		cnt = 0;
+    cin >> T;
+    while (T--)
+    {
+        fill(&a[0][0], &a[0][0] + 54 * 54, 0); // 한 테스트 케이스 끝나고 a 배열, visited 배열 초기화 해야함
+        fill(&visited[0][0], &visited[0][0] + 54 * 54, 0); // 이차원 배열 초기화 fill 함수 사용법 암기
 
-		for (int i = 0; i < 51; i++)
-		{
-			for (int j = 0; j < 51; j++)
-			{
-				m_map[i][j] = 0;
-				visited[i][j] = 0;
-			}
-		}
+        int res = 0;
+        cin >> m >> n >> k;
 
-		cin >> n >> m >> k;
+        for (int i = 0; i < k; i++)
+        {
+            int x, y;
+            cin >> x >> y;
+            a[y][x] = 1;
+        }
 
-		for (int i = 0; i < k; i++)
-		{
-			int a, b;
-			cin >> a >> b;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (a[i][j] == 1 && !visited[i][j])
+                {
+                    go(i, j);
+                    res++;
+                }
+            }
+        }
 
-			m_map[a][b] = 1;
-		}
-		
-		/*for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < m; j++)
-			{
-				cout << m_map[i][j] << " ";
-			}
-			cout << "\n";
-		}*/
-
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < m; j++)
-			{
-				if (m_map[i][j] == 1 && !visited[i][j])
-				{
-					cnt++;
-					DFS(i, j);
-				}
-			}
-		}
-
-		cout << cnt << "\n";
-	}
-
-	return 0;
+        cout << res << '\n';
+    }
+    
+    return 0;
 }
