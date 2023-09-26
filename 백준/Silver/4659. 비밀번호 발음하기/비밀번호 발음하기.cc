0c73008a0dec8d0d28a22ec90e0006f7d1fdd950
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2023-09-26 17:27:24 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
@@ -1,13 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
+#include <string>
 
 using namespace std;
-
-bool isVowel(int idx)
-{
-	return (idx == 'a' || idx == 'e' || idx == 'i' || idx == 'o' || idx == 'u');
-}
 
 int main()
 {
@@ -19,38 +16,53 @@ int main()
 		string str;
 		cin >> str;
 
-		if (str == "end") break;
+		if (str == "end") return 0;
 
-		bool flag = false, is_include_v = false;
-		int constant = 0, vowel= 0, prev = -1;
+		vector<int> vec;
+		bool isAcceptable = true; // 비밀번호 가능 여부 체크
+		bool exist = false; // 모음 존재
+		int vowel = 0, constants = 0; // 자음, 모음 수 체크
 
 		for (int i = 0; i < str.size(); i++)
 		{
-			int idx = str[i];
-			if (isVowel(idx))
+			// 모음 체크
+			if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
 			{
+				exist = true;
 				vowel++;
-				constant = 0;
-				is_include_v = true;
+				constants = 0;
 			}
 			else
 			{
-				constant++;
+				constants++;
 				vowel = 0;
 			}
 
-			if (vowel == 3 || constant == 3) flag = 1;
+			// 3개 이상 연속 체크
+			if (constants == 3 || vowel == 3)
+			{
+				isAcceptable = false;
+				break;
+			}
 
-			if (i >= 1 && (prev == idx) && (idx != 'e' && idx != 'o')) flag = 1;
-
-			prev = idx;
+			// 같은 글자 연속 여부 확인
+			// 범위 지정 이후 index i와 i+1 이용해서 검사
+			if (i < str.size() - 1)
+			{
+				if (str[i] == 'e' && str[i + 1] == 'e' || str[i] == 'o' && str[i + 1] == 'o') continue;
+				else if (str[i] == str[i + 1])
+				{
+					isAcceptable = false;
+					break;
+				}
+			}
+	
 		}
 
-		if (!is_include_v) flag = 1;
+		if (exist && isAcceptable) cout << "<" << str << "> is acceptable." << "\n";
+		else cout << "<" << str << "> is not acceptable." << "\n";
 
-		if (flag) cout << "<" << str << "> is not acceptable.\n";
-		else cout << "<" << str << "> is acceptable.\n";
 	}
-	
+
 	return 0;
 }
