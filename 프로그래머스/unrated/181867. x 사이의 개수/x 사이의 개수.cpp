a1d40700285efb9edffe1c0b321a49ea9b2843d1
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2023-10-23 15:15:11 +0900

    [level unrated] Title: x 사이의 개수, Time: 3.34 ms, Memory: 6.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(string myString) {
+    vector<int> answer;
+    
+    int cnt = 0;
+    for(int i = 0; i < myString.size(); i++)
+    {
+        if(myString[i] == 'x')
+        {
+            answer.push_back(cnt);
+            cnt = 0;
+        }
+        else cnt++;
+    }
+    answer.push_back(cnt);
+    
+    return answer;
+}
