71756bcd5c719491a2e88b9951cc5743070e99fe
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2023-10-27 15:06:39 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,44 +1,50 @@
-#include <iostream>
-#include <vector>
-# define MAX 8
- 
+// BOJ-15649 : N과 M
+#include<bits/stdc++.h>
 using namespace std;
- 
-int arr[MAX]; //주어진 숫자
-bool visited[MAX]; //방문, 중복 방지
-vector<int> vec; //출력할 수열
-int cnt, n, m;
- 
-void Print() {
-    for (int i = 0; i < vec.size(); i++)
-        cout << vec[i] << " ";
-    cout << '\n';
-    return;
+
+const int N = 8;
+int n, m, a[N], visited[N];
+vector<int> v;
+
+void printV()
+{
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << ' ';
+	}
+	cout << '\n';
+	return;
 }
- 
-void DFS(int cnt) {
-    if (cnt == m) {
-        Print();
-        return;
-    }
- 
-    for (int i = 0; i < n; i++) {
-        if (visited[i]) continue;
-        visited[i] = true;
-        vec.push_back(arr[i]);
-        DFS(cnt + 1);
-        vec.pop_back();
-        visited[i] = false;
-    }
+
+void go(int cnt)
+{
+	if (cnt == m)
+	{
+		printV();
+		return;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		if (visited[i]) continue;
+		visited[i] = 1;
+		v.push_back(a[i]);
+		go(cnt + 1);
+		visited[i] = 0;
+		v.pop_back();
+	}
 }
- 
-int main(void) {
-    cin >> n >> m;
- 
-    for (int i = 0; i < n; i++) {
-        arr[i] = i + 1;
-        visited[i] = false;
-    }
- 
-    DFS(0);
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) a[i] = i + 1;
+
+	go(0);
+	
+	return 0;
 }
