2093e4ff0752e59a11191a494f594d8e4e70794b
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2023-10-28 21:46:35 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 12 ms, Memory: 3332 KB -BaekjoonHub

---
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
@@ -0,0 +1,51 @@
+// 13913 : 숨바꼭질 4
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX = 100000;
+int n, k;
+int visited[MAX + 4], pos[MAX+4];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> k;
+
+    queue<int> q;
+
+    if (n == k)
+    {
+        cout << "0\n" << k << '\n';
+        return 0;
+    }
+
+    q.push(n);
+    visited[n] = 1;
+    pos[n] = n;
+
+    while (q.size())
+    {
+        int now = q.front();
+        q.pop();
+
+        for (int next : {now - 1, now + 1, now * 2})
+        {
+            if (next <0 || next > MAX) continue;
+            if (visited[next]) continue;
+
+            visited[next] = visited[now] + 1;
+            pos[next] = now;
+            q.push(next);
+        }
+    }
+
+    cout << visited[k] - 1 << '\n';
+
+    deque<int> dq = { k };
+    while (dq.front() != n) dq.push_front(pos[dq.front()]);
+
+    for (int i : dq) cout << i << ' ';
+
+    return 0;
+}
