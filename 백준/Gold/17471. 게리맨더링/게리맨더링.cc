fae4bbabbc31eac06ff9b36009d5dc0f0f38ede8
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2023-11-17 15:12:59 +0900

    [Gold IV] Title: 게리맨더링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.cc
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.cc
@@ -1,15 +1,8 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <queue>
-#include <cmath>
-#include <map>
-#include <string>
-
+// BOJ-17471 : 게리멘더링
+#include<bits/stdc++.h>
 using namespace std;
 
 const int INF = 2147000000;
-
 int n, m, res = INF;
 int population[11], comp[11], visited[11];
 
@@ -18,7 +11,7 @@ vector<int> adj[11];
 pair<int, int> dfs(int here, int value)
 {
 	visited[here] = 1;
-	pair<int, int> res = { 1, population[here] };
+	pair<int, int> res = { 1, population[here] }; 
 	for (int there : adj[here])
 	{
 		if (comp[there] != value) continue;
@@ -37,50 +30,43 @@ int main()
 
 	cin >> n;
 
-	for (int i = 1; i <= n; i++)
-	{
-		cin >> population[i];
-	}
+	for (int i = 1; i <= n; i++) cin >> population[i];
 
 	for (int i = 1; i <= n; i++)
 	{
 		cin >> m;
 		for (int j = 0; j < m; j++)
 		{
-			int a;
-			cin >> a;
-			adj[i].push_back(a);
-			adj[a].push_back(i);
+			int tmp;
+			cin >> tmp;
+			adj[tmp].push_back(i);
+			adj[i].push_back(tmp);
 		}
 	}
 
-	// 비트마스킹을 통해 모든 경우의 수 체크, 0과 1<<n 은 One-side 이기 때문에 제외
 	for (int i = 1; i < (1 << n) - 1; i++)
 	{
-		// Red, Blue 구역 색칠 초기화
 		fill(comp, comp + 11, 0);
-		// DFS 방문 지역 초기화
 		fill(visited, visited + 11, 0);
 
-		// dfs 시작 지점
 		int idx1 = -1, idx2 = -1;
 
 		for (int j = 0; j < n; j++)
 		{
-			if (i & (1 << j)) {
+			if (i & (1 << j))
+			{
 				comp[j + 1] = 1;
 				idx1 = j + 1;
 			}
 			else idx2 = j + 1;
 		}
-
 		pair<int, int> comp1 = dfs(idx1, 1);
 		pair<int, int> comp2 = dfs(idx2, 0);
 
 		if (comp1.first + comp2.first == n) res = min(res, abs(comp1.second - comp2.second));
 	}
 
-	cout << (res == INF ? -1 : res) << "\n";
+	cout << (res == INF ? -1 : res) << '\n';
 
 	return 0;
 }
