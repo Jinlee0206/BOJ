4e1c6c5b0eaf9bac6723f7e4f5956399f9d386b5
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2023-11-21 18:38:53 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -1,31 +1,27 @@
-// 백트래킹, DFS
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int cnt = 0, answer = 0;
-string aeiou = "AEIOU";
-
-void dfs(string word, string str)
-{
-    // 기저 사례
-    if(str == word) // 단어를 찾으면 answer에 cnt 할당
-    {
-        answer = cnt;
-        return;
-    }
-    
-    for(int i = 0; i< 5; i++)
-    {
-        if(str.size() < 5)
-        {
-            cnt++; // 알파벳을 하나 씩 붙이는 과정에서 cnt 증가
-            dfs(word, str + aeiou[i]); // word의 길이는 1이상 5이하
-        }
-    }
-}
-
 int solution(string word) {
-    dfs(word, "");
+    int answer = 0;
+    vector<int> v
+    {
+        781, 156, 31, 6, 1
+    };
+
+    map<char, int> m
+    {
+        make_pair('A', 1),
+        make_pair('E', 2),
+        make_pair('I', 3),
+        make_pair('O', 4),
+        make_pair('U', 5),
+    };
+
+    for(int i = 0; i < word.size(); i++)
+    {
+        answer += (m[word[i]] - 1) * v[i] + 1;
+    }
+
     return answer;
 }
