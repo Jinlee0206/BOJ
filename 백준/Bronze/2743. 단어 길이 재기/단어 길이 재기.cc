71045955bcd34d9f8ca2128e6d4413c90ba5deca
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-01-02 17:40:55 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string str;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> str;
+	cout << str.size();
+
+	return 0;
+}
