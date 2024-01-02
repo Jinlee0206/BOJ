cfd127e0f1b4ce97ee09f9e483199160240a915f
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-01-02 17:39:29 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string str;
+int n;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> str >> n;
+	cout << str[n - 1];
+
+	return 0;
+}
