2d9956cc0e513a18e20686166ecdf2622d764032
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-01-23 15:14:38 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int a, int b, int c) {
+    int answer = 0;
+    if(a == b && b == c && c == a)
+    {
+        answer = (a + b + c) * (pow(a,2) + pow(b,2) + pow(c,2)) * (pow(a,3) + pow(b,3) + pow(c,3)); 
+    }
+    else if((a == b && a != c) || (b == c && b != a) || (c == a && c != b))
+    {
+        answer = (a + b + c) * (pow(a,2) + pow(b,2) + pow(c,2));
+    }
+    else
+    {
+        answer = a + b + c;
+    }
+    return answer;
+}
