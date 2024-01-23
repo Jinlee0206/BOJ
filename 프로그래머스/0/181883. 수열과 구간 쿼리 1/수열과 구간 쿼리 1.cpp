e32b0bb1572904cb001892173ee7cdf3c28f7d10
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-01-23 13:43:41 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 0.18 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    vector<int> answer(arr.begin(), arr.end());
+    
+    for(int i = 0; i< queries.size(); i++)
+    {
+        int s = queries[i][0];
+        int e = queries[i][1];
+        for(int j = s; j<= e; j++)
+        {
+            answer[j]++;
+        }
+    }
+    
+    return answer;
+}
