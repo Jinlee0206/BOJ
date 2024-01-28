c3ade15b405a9c3820bf2ff8dbaabe172c18d2eb
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-01-28 16:38:35 +0900

    [Gold III] Title: 순회강연, Time: 0 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.cc
+++ 백준/Gold/2109. 순회강연/순회강연.cc
@@ -1,18 +1,11 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <queue>
-
+// BOJ-2109 : 순회강연
+#include<bits/stdc++.h>
 using namespace std;
 
-int n, d, p, max = -2147000000, res = 0;
+const int INF = 2147000000;
+int n, p, d, mx = -INF, res;
 
-// 1. day 내림차순 정렬 방법
-
-/*
-struct Data
-{
+struct Data {
 	int money;
 	int when;
 
@@ -22,11 +15,9 @@ struct Data
 		when = b;
 	}
 
-	bool operator<(const Data& b) const
-	{
+	bool operator< (const Data& b) const {
 		return when > b.when;
 	}
-
 };
 
 int main()
@@ -37,36 +28,27 @@ int main()
 	vector<Data> v;
 	priority_queue<int> pq;
 
-
 	cin >> n;
-
 	for (int i = 0; i < n; i++)
 	{
 		cin >> p >> d;
 		v.push_back(Data(p,d));
 
-		if (d > max)
-		{
-			max = d;
-		}
+		mx = max(mx, d);
 	}
 
 	sort(v.begin(), v.end());
 
 	int j = 0;
-	for (int i = max; i >= 1; i--)
+	for (int i = mx; i >= 1; i--)
 	{
 		for (; j < n; j++)
 		{
-			//cout << "I_position : " << i << ' ';
-			//cout << "J_position : " << j << '\n';
-			
-			// break 구문은 반복자를 ++시키지 않는다
 			if (v[j].when < i) break;
 			pq.push(v[j].money);
 		}
 
-		if (!pq.empty())
+		if (pq.size())
 		{
 			res += pq.top();
 			pq.pop();
@@ -74,40 +56,5 @@ int main()
 	}
 
 	cout << res << '\n';
-		
-	return 0;
-}
-*/
-
-// 2. Day 오름차순 정렬
-int main()
-{
-	priority_queue<int, vector<int>, greater<int>> pQ;
-	vector<pair<int, int>> v;
-
-	cin >> n;
-
-	for (int i = 0; i < n; i++)
-	{
-		cin >> p >> d;
-		v.push_back({ d,p });
-	}
-
-	sort(v.begin(), v.end());
-
-	for (int i = 0; i < n; i++)
-	{
-		pQ.push(v[i].second);
-		if (pQ.size() > v[i].first) pQ.pop();
-	}
-
-	while (pQ.size())
-	{
-		res += pQ.top();
-		pQ.pop();
-	}
-
-	cout << res << '\n';
-
 	return 0;
 }
