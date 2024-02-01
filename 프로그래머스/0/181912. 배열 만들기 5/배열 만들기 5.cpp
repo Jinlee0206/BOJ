d37ae8f52201fccf6ffd9a79b68a396c88881bc2
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-02-01 10:50:18 +0900

    [level 0] Title: 배열 만들기 5, Time: 1.08 ms, Memory: 5.51 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<string> intStrs, int k, int s, int l) {
+    vector<int> answer;
+    
+    for(int i = 0; i< intStrs.size(); i++)
+    {
+        int tmp = stoi(intStrs[i].substr(s, l));
+        if(tmp > k) answer.push_back(tmp);
+    }
+    return answer;
+}
