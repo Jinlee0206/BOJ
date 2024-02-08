1ab28bf140a11e2f53d9eff3caa2e405c01f55f8
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-02-08 15:16:10 +0900

    [level 0] Title: k의 개수, Time: 0.24 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/k의 개수.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int i, int j, int k) {
+    int answer = 0;
+    
+    for(int l = i; l <= j; l++)
+    {
+        int tmp = l;
+        while(tmp > 0)
+        {
+            if(tmp % 10 == k) answer++;
+            tmp /= 10;
+        }
+    }
+    return answer;
+}
