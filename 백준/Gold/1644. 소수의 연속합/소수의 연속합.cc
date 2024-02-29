e7ae59ed5e2b70e50089cdd8ed07619589d7972f
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-02-29 17:21:40 +0900

    [Gold III] Title: 소수의 연속합, Time: 24 ms, Memory: 9128 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -1,13 +1,12 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+// BOJ-1644 : 소수의 연속합
+#include <bits/stdc++.h>
 using namespace std;
 
 int n;
 vector<int> pn;
 bool ch[4000001];
 
+// 에라스토테네스의 체
 void makePN(int a)
 {
 	for (int i = 2; i * i <= a; i++)
@@ -18,37 +17,28 @@ void makePN(int a)
 			ch[j] = true;
 		}
 	}
-
 	for (int i = 2; i <= a; i++) if (!ch[i]) pn.push_back(i);
-
-	return;
 }
-
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-
+	
 	cin >> n;
 
 	makePN(n);
-	
-	//for (int i = 0; i < pn.size(); i++)
-	//{
-	//	cout << pn[i] << ' ';
-	//}
 
-	int p1, p2, sum = 0, cnt = 0;
+	// 투포인터 알고리즘
+	int p1, p2, sum = 0, cnt= 0;
 	p1 = p2 = 0;
-
-	while (1)
+	while (true)
 	{
 		if (sum >= n) sum -= pn[p1++];
 		else if (p2 == pn.size()) break;
 		else sum += pn[p2++];
 
-		if (sum == n) cnt++;
+		if (n == sum) cnt++;
 	}
 
 	cout << cnt << '\n';
