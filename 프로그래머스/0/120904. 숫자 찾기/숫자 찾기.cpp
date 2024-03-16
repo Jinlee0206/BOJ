63f2368bc52366450a4a4b456b2477932653fb28
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-03-17 00:44:54 +0900

    [level 0] Title: 숫자 찾기, Time: 0.01 ms, Memory: 3.71 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int num, int k) {
+    int answer = 0;
+    
+    string str = to_string(num);
+    for(int i = 0; i< str.size(); i++)
+    {
+        if(str[i] - '0' == k) 
+        {
+            answer = i + 1;
+            break;
+        }
+    }
+    
+    if(answer == 0) answer = -1; 
+    
+    return answer;
+}
