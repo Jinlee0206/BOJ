1fe4cb647748f25db6c272c6c1ac9d89c0e2231e
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-03-17 16:47:33 +0900

    [Silver III] Title: 두 수의 합, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -1,46 +1,37 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+// BOJ-3273 : 두 수의 합
+#include <bits/stdc++.h>
 using namespace std;
 
-int n, k, cnt;
-vector<int> v;
+int n, a[100004], res, x;
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-
+	
 	cin >> n;
-
 	for (int i = 0; i < n; i++)
 	{
-		int a;
-		cin >> a;
-		v.push_back(a);
+		cin >> a[i];
 	}
 
-	cin >> k;
+	cin >> x;
 
-	sort(v.begin(), v.end());
+	sort(a, a + n);
 
-	int p1, p2;
-	p1 = 0, p2 = v.size() - 1;
+	int p1 = 0, p2 = n - 1;
 
 	while (p1 < p2)
 	{
-		if (v[p1] + v[p2] == k)
+		if (a[p1] + a[p2] < x) p1++;
+		else if (a[p1] + a[p2] == x)
 		{
+			res++;
 			p1++;
-			p2--;
-			cnt++;
 		}
-		else if (v[p1] + v[p2] > k) p2--;
-		else if (v[p1] + v[p2] < k) p1++;
+		else p2--;
 	}
 
-	cout << cnt << '\n';
-
+	cout << res << '\n';
 	return 0;
 }
