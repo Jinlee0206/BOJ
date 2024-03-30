225d0c3423bf48456fe02c88c3ffab27c1bc4405
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-03-30 14:56:24 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -1,52 +1,51 @@
+// BOJ - 14002 : 가장 긴 증가하는 부분 수열 4
 #include <bits/stdc++.h>
-
 using namespace std;
 
-// LIS (prev_list 활용)
-
-int n, a[1001], res = 1, cnt[1001], idx;
-int prev_list[1001];
+const int INF = 2147000000;
+int n, a[1004], cnt[1004], res = 1, idx;
+int prev_list[1004];
 vector<int> v;
 
 void go(int idx)
 {
-    if (idx == -1) return;
-    v.push_back(a[idx]);
-    go(prev_list[idx]);
-    return;
+	if (idx == -1) return;
+	v.push_back(a[idx]);
+	go(prev_list[idx]);
+	return;
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
-    cin >> n;
-    for (int i = 0; i < n; i++) cin >> a[i];
-    fill(cnt, cnt + 1001, 1);
-    fill(prev_list, prev_list + 1001, -1);
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            if (a[j] < a[i] && cnt[i] < cnt[j] + 1)
-            {
-                cnt[i] = cnt[j] + 1;
-                prev_list[i] = j;
-                if (res < cnt[i])
-                {
-                    res = cnt[i];
-                    idx = i;
-                }
-            }
-        }
-    }
+	cin >> n;
 
-    cout << res << '\n';
-    go(idx);
-    for (int i = v.size() - 1; i >= 0; i--) cout << v[i] << " ";
-    //cout << '\n';
-    //for (int i = 0; i < n; i++) cout << prev_list[i] << " ";
+	for (int i = 0; i < n; i++) cin >> a[i];
+	fill(prev_list, prev_list + 1004, -1);
+	fill(cnt, cnt + 1004, 1);
 
-    return 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (a[j] < a[i] && cnt[i] < cnt[j] + 1)
+			{
+				cnt[i] = cnt[j] + 1;
+				prev_list[i] = j;
+				if (res < cnt[i])
+				{
+					res = cnt[i];
+					idx = i;
+				}
+			}
+		}
+	}
+
+	cout << res << '\n';
+	go(idx);
+	for (int i = v.size() - 1; i >= 0; i--) cout << v[i] << " ";
+
+	return 0;
 }
