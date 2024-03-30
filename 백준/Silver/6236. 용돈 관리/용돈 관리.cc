be11e53491608668402e630541345cc46673e746
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-03-30 18:35:30 +0900

    [Silver II] Title: 용돈 관리, Time: 12 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.cc
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.cc
@@ -2,13 +2,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-ll n, m, mx, l = 1, h, mid, rets = 1e18;
+ll n, m, mx, l = 1, r, mid, res = 1e18;
 ll a[100004];
 
 bool go() {
-    if (mx > mid)
-        return false;
-    int num = 1, sum = mid;
+    if (mx > mid) return false;
+    int num = 1;
+    ll sum = mid;
     
     for (int i = 0; i < n; i++) {
         if (sum - a[i] < 0) {
@@ -33,17 +33,17 @@ int main() {
         mx = max(mx, a[i]);
     }
     
-    l = 1, h = sum;
+    l = 1, r = sum;
     
-    while (l <= h) {
-        mid = (h + l) / 2;
+    while (l <= r) {
+        mid = (r + l) / 2;
         if (go()) {
-            h = mid - 1;
-            rets = min(rets, mid);
+            r = mid - 1;
+            res = min(res, mid);
         }
         else l = mid + 1;
     }
-    cout << rets << "\n";
+    cout << res << "\n";
 
     return 0;
 }
