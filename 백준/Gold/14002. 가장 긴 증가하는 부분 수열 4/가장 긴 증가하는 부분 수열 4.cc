68bd2e2853e9898e8b8682c037e4234adf4d68dc
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-04-04 15:47:41 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -1,51 +1,44 @@
-// BOJ - 14002 : 가장 긴 증가하는 부분 수열 4
+// BOJ - 14002 : 가장 긴 증가하는 부분수열 4
 #include <bits/stdc++.h>
 using namespace std;
 
-const int INF = 2147000000;
-int n, a[1004], cnt[1004], res = 1, idx;
-int prev_list[1004];
-vector<int> v;
-
-void go(int idx)
-{
-	if (idx == -1) return;
-	v.push_back(a[idx]);
-	go(prev_list[idx]);
-	return;
-}
+int n, lis[1004], len;
+pair<int, int> a[1004];
+stack<int> stk;
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-
+	
 	cin >> n;
-
-	for (int i = 0; i < n; i++) cin >> a[i];
-	fill(prev_list, prev_list + 1004, -1);
-	fill(cnt, cnt + 1004, 1);
 
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < i; j++)
+		int num;
+		cin >> num;
+		auto lowerPos = lower_bound(lis, lis + len, num);
+		if (*lowerPos == 0) len++;
+		*lowerPos = num;
+		a[i].first = (int)(lowerPos - lis);
+		a[i].second = num;
+	}
+	cout << len << '\n';
+
+	for (int i = n - 1; i >= 0; i--)
+	{
+		if (a[i].first == len - 1)
 		{
-			if (a[j] < a[i] && cnt[i] < cnt[j] + 1)
-			{
-				cnt[i] = cnt[j] + 1;
-				prev_list[i] = j;
-				if (res < cnt[i])
-				{
-					res = cnt[i];
-					idx = i;
-				}
-			}
+			stk.push(a[i].second);
+			len--;
 		}
 	}
 
-	cout << res << '\n';
-	go(idx);
-	for (int i = v.size() - 1; i >= 0; i--) cout << v[i] << " ";
+	while (stk.size())
+	{
+		cout << stk.top() << ' ';
+		stk.pop();
+	}
 
 	return 0;
 }
