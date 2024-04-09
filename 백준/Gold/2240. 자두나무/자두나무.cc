751876276a6267f4982bf602de2b0f7d9c01247b
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-04-10 02:12:37 +0900

    [Gold V] Title: 자두나무, Time: 0 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.cc
+++ 백준/Gold/2240. 자두나무/자두나무.cc
@@ -1,33 +1,31 @@
 // BOJ - 2240 : 자두나무
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+const int INF = 2147000000;
 
-int dp[1004][2][34], n, m, b[1004];
+int n, m, dp[1004][2][34], b[1004];
 
 int go(int idx, int tree, int cnt)
 {
-	// 1. 기저사례
-	if (cnt < 0) return -1e9;
-	if (idx == n) return cnt == 0 ? 0 : -1e9;
-	// 2. 메모이제이션
-	int& res = dp[idx][tree][cnt];
-	if (~res) return res;
-	// 3. 로직
-	return res = max(go(idx + 1, tree^1, cnt - 1), go(idx + 1, tree, cnt)) + (tree == b[idx] - 1);
+    if (cnt < 0) return -1e9;
+    if (idx == n) return cnt == 0 ? 0 : -1e9;
+
+    int& res = dp[idx][tree][cnt];
+    if (res != -1) return res;
+
+    return res = max(go(idx + 1, tree ^ 1, cnt - 1), go(idx + 1, tree, cnt)) + (tree == b[idx] - 1);
 }
 
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	// 4. 초기화
-	memset(dp, -1, sizeof(dp)); 
-	
-	cin >> n >> m;
+    memset(dp, -1, sizeof(dp));
 
-	for (int i = 0; i < n; i++) cin >> b[i];
-	cout << max(go(0, 1, m - 1), go(0, 0, m)) << '\n';
+    cin >> n >> m;
 
-	return 0;
+    for (int i = 0; i < n; i++) cin >> b[i];
+    cout << max(go(0, 1, m - 1), go(0, 0, m)) << '\n';
+
+    return 0;
 }
