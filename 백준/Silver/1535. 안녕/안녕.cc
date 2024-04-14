b1700844478e8ab9a3cf275e9f06e4a707ef057d
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-04-14 19:21:20 +0900

    [Silver II] Title: 안녕, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.cc
+++ 백준/Silver/1535. 안녕/안녕.cc
@@ -1,27 +1,33 @@
 // BOJ - 1535 : 안녕
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-const int INF = 2147000000;
+int n, dp[101], L[101], J[101];
 
-int n, a, dp[101], cost[101], happy[101];
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+	cin >> n;
 
-    cin >> n;
+	for (int i = 0; i < n; i++) cin >> L[i];
+	for (int i = 0; i < n; i++) cin >> J[i];
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 100; j > L[i]; j--)
+		{
+			dp[j] = max(dp[j], dp[j - L[i]] + J[i]);
+		}
 
-    for (int i = 0; i < n; i++) cin >> cost[i];
-    for (int i = 0; i < n; i++) cin >> happy[i];
+		/*
+		for (int l = 0; l < 100; l++)
+		{
+			cout << dp[l] << ' ';
+		}
+		cout << '\n';
+		*/
+	}
+	cout << dp[100] << '\n';
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 100; j > cost[i]; j--)
-        {
-            dp[j] = max(dp[j], dp[j - cost[i]] + happy[i]);
-        }
-    }
-    cout << dp[100] << '\n';
-
-    return 0;
+	return 0;
 }
