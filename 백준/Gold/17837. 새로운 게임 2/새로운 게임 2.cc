d67fe48c8399f0defb9a6a06ede3d38eed7605df
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-04-24 00:41:28 +0900

    [Gold II] Title: 새로운 게임 2, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.cc
+++ 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.cc
@@ -7,34 +7,33 @@ const int dy[] = { 0, 0, -1, 1 };
 const int dx[] = { 1, -1, 0 ,0 };
 
 int n, k, a[15][15];
+vector<int> v[15][15];
 struct Point {
     int y, x, dir;
 };
-vector<int> v[15][15];
 vector<Point> status;
 
-void move(int y, int x, int dir, int i)
+void move(int y, int x, int dir, int idx)
 {
     int ny = y + dy[dir];
     int nx = x + dx[dir];
 
-    // 파란색 or 체스판을 벗어나는 경우
     if (ny < 0 || nx < 0 || ny >= n || nx >= n || a[ny][nx] == 2)
     {
-        status[i].dir ^= 1; // 방향 전환
-        ny = y + dy[status[i].dir];
-        nx = x + dx[status[i].dir];
+        status[idx].dir ^= 1;
+        ny = y + dy[status[idx].dir];
+        nx = x + dx[status[idx].dir];
         if (ny < 0 || nx < 0 || ny >= n || nx >= n || a[ny][nx] == 2) return;
     }
-    // 한칸 이동 로직
-    vector<int>& here_v = v[y][x];   // 현 위치
-    vector<int>& next_v = v[ny][nx]; // 다음 위치
-    auto pos = find(here_v.begin(), here_v.end(), i);
-    // 뒤집어서 보내기
+
+    vector<int>& here_v = v[y][x]; // 현위치
+    vector<int>& next_v = v[ny][nx]; // 다음위치
+    auto pos = find(here_v.begin(), here_v.end(), idx);
+
     if (a[ny][nx] == 1) reverse(pos, here_v.end());
     for (auto it = pos; it != here_v.end(); it++)
     {
-        next_v.push_back(*it);
+        next_v.push_back(*it); // iterator는 주소값, 역참조연산자로 인덱스 value 얻을 수 있다
         status[*it].y = ny;
         status[*it].x = nx;
     }
@@ -90,23 +89,22 @@ int main() {
         cin >> y >> x >> dir;
         y--; x--; dir--;
         v[y][x].push_back(i);
-        status.push_back({ y,x,dir }); // 말의 상태값을 저장
+        status.push_back({ y,x,dir }); // 말의 상태값 저장
     }
 
     int cnt = 0;
-    bool flag = 0;
+    bool flag = false;
     while (cnt <= 1000)
     {
         cnt++;
         if (simul())
         {
-            flag = 1;
+            flag = true;
             break;
         }
     }
-
-    if (flag) cout << cnt << '\n';
-    else cout << -1 << '\n';
+    if (!flag) cout << -1 << '\n';
+    else cout << cnt << '\n';
 
     return 0;
 }
