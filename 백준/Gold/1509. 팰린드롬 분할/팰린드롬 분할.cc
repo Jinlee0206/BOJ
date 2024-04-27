3eae45842b975ebeb06c059aea0d2ee56ffc1ad7
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-04-27 21:50:53 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 56 ms, Memory: 26528 KB -BaekjoonHub

--- 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.cc
@@ -1,46 +1,46 @@
 // BOJ - 1509 : 팰린드롬 분할
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 const int INF = 2147000000;
-int dp[2504][2504]; // 시작지점과 팰린드롬의 사이즈를 상태값으로 가짐
-int dp2[2504];		// 분할의 최소개수를 찾기위한 dp배열
-string str;
+
+int dp[2504][2504];
+int dp2[2504];
+string s;
 
 int go(int here)
 {
-	if (here == str.size()) return 0;
-	if (dp2[here] != INF) return dp2[here];
-	int& res = dp2[here];
-	for (int i = 1; i + here <= str.size(); i++)
-	{
-		if (dp[here][i]) res = min(res, go(here + i) + 1);
-	}
-	// cout << here << " : " << res << '\n';
-	return res;
+    if (here == s.size()) return 0;
+    if (dp2[here] != INF) return dp2[here];
+    int& res = dp2[here];
+    for (int i = 1; i + here <= s.size(); i++)
+    {
+        if (dp[here][i]) res = min(res, go(here + i) + 1);
+    }
+    //cout << here << " : " << res << '\n';
+    return res;
 }
 
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	cin >> str;
+    cin >> s;
 
-	for (int i = 0; i < str.size(); i++) dp[i][1] = 1;
-	for (int i = 0; i < str.size() - 1; i++)
-	{
-		if (str[i] == str[i + 1]) dp[i][2] = 1;
-	}
+    for (int i = 0; i < s.size(); i++) dp[i][1] = 1;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == s[i + 1]) dp[i][2] = 1;
+    }
+    for (int _size = 3; _size <= s.size(); _size++)
+    {
+        for (int j = 0; j + _size <= s.size(); j++)
+        {
+            if (s[j] == s[j + _size - 1] && dp[j + 1][_size - 2]) dp[j][_size] = 1;
+        }
+    }
 
-	for (int _size = 3; _size <= str.size(); _size++)
-	{
-		for (int j = 0; j + _size <= str.size(); j++)
-		{
-			if (str[j] == str[j + _size - 1] && dp[j + 1][_size - 2]) dp[j][_size] = 1;
-		}
-	}
-	fill(dp2, dp2 + 2504, INF);
-	cout << go(0) << '\n';
+    fill(dp2, dp2 + 2504, INF);
+    cout << go(0) << '\n';
 
-	return 0;
+    return 0;
 }
