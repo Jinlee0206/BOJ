6254c4676679034fc42be54898642f17ccca60ba
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-04-28 19:44:09 +0900

    [Gold I] Title: 냅색문제, Time: 4 ms, Memory: 2408 KB -BaekjoonHub

--- 백준/Gold/1450. 냅색문제/냅색문제.cc
+++ 백준/Gold/1450. 냅색문제/냅색문제.cc
@@ -1,25 +1,21 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+// BOJ - 1450 : 냅색문제
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 
-ll w[31], ans, n, c;
-vector<ll> group1, group2;
+int n, c, a[34], res;
+vector<int> v, v2;
 
-void DFS(int start, int end, vector<ll>& vec, ll sum)
+void go(int here, int _n, vector<int>& v, int sum)
 {
-	if (start > end)
+	if (sum > c) return;
+	if (here > _n)
 	{
-		vec.push_back(sum);
+		v.push_back(sum);
 		return;
 	}
-	else
-	{
-		DFS(start + 1, end, vec, sum); // start 번째의 물건을 선택하지 않는 경우
-		DFS(start + 1, end, vec, sum + w[start]);  // start 번째의 물건을 선택하는 경우
-	}
+	go(here + 1, _n, v, sum + a[here]); 
+	go(here + 1, _n, v, sum);
+	return;
 }
 
 int main()
@@ -29,25 +25,20 @@ int main()
 
 	cin >> n >> c;
 
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++) cin >> a[i];
+
+	go(0, n / 2 - 1, v, 0);
+	go(n / 2, n - 1, v2, 0);
+	sort(v.begin(), v.end());
+	sort(v2.begin(), v2.end());
+
+	for (int b : v)
 	{
-		cin >> w[i];
+		if (c - b >= 0)
+		{
+			res += ((int)(upper_bound(v2.begin(), v2.end(), c - b) - v2.begin()));
+		}
 	}
-
-	DFS(0, n / 2, group1, 0);
-	DFS(n / 2 + 1, n - 1, group2, 0);
-
-	sort(group2.begin(), group2.end());
-
-	for (int i = 0; i < group1.size(); i++)
-	{
-		// upper_bound() : c - group[i] 값을 초과하는 숫자가 group2 배열의 몇 번째에서 처음 등장하는지 알 수 있음. iterator 반환
-		// lower_bound() : c - group[i] 값과 같거나 큰 숫자가 group2 배열의 몇 번째에서 처음 등장하는지 알 수 있음. iterator 반환
-		ll x = c - group1[i];
-		ans += upper_bound(group2.begin(), group2.end(), x) - group2.begin();
-	}
-
-	cout << ans << "\n";
-
+	cout << res << '\n';
 	return 0;
 }
