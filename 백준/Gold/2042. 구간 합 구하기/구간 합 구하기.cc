e9796d538b516cf40c0bc0b57739fdc7470b47fe
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-05-12 19:50:46 +0900

    [Gold I] Title: 구간 합 구하기, Time: 228 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,19 +1,18 @@
-// BOJ - 2042 : 구간 합 구하기
+// BOJ - 2042 : 구간 합 구하기 (팬윅트리)
 #include <bits/stdc++.h>
 using namespace std;
-const int INF = 2147000000;
 typedef long long ll;
 
 int n, m, k, t1, t2, t3;
 ll t4;
 
-ll sum(vector<ll>& tree, int idx)
+ll sum(vector<ll>& tree, int i)
 {
 	ll ans = 0;
-	while (idx > 0)
+	while (i > 0)
 	{
-		ans += tree[idx];
-		idx -= (idx & -idx);
+		ans += tree[i];
+		i -= (i & -i);
 	}
 	return ans;
 }
@@ -39,19 +38,6 @@ int main()
 	{
 		cin >> a[i];
 		update(tree, i, a[i]);
-		/*for (int j = 1; j < tree.size(); j++)
-		{
-			cout << tree[j] << ' ';
-		}
-		cout << "\n";
-		*/
-		///
-		/// i = 1 : 1 1 0 1 0
-		/// i = 2 : 1 3 0 3 0 
-		/// i = 3 : 1 3 3 6 0
-		/// i = 4 : 1 3 3 10 0
-		/// i = 5 : 1 3 3 10 5
-		/// 
 	}
 	m += k;
 	while (m--)
@@ -60,8 +46,8 @@ int main()
 		if (t1 == 1)
 		{
 			cin >> t2 >> t4;
-			ll diff = t4 - a[t2]; // 변경 될 값과 원래 값과의 차이를 계산
-			a[t2] = t4; // 값 원본배열에 갱신
+			ll diff = t4 - a[t2];
+			a[t2] = t4;
 			update(tree, t2, diff);
 		}
 		else
