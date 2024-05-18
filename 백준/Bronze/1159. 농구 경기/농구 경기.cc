116471a9dc6ef35dde97366a80fa452b64962667
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-05-18 15:42:55 +0900

    [Bronze II] Title: 농구 경기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.cc
+++ 백준/Bronze/1159. 농구 경기/농구 경기.cc
@@ -1,31 +1,31 @@
+// BOJ - 1159 : 농구경기
+#define _CRT_SECURE_NO_WARNINGS
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int alpha[26];
+int n, alpha[26];
+string ans = "";
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> n;
 
-    int n;
-    cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		string str;
+		cin >> str;
+		alpha[str[0] - 'a']++;
+	}
 
-    for (int i = 0; i < n; i++)
-    {
-        string str;
-        cin >> str;
-        alpha[str[0] - 'a']++;
-    }
-
-    string answer;
-    for (int i = 0; i < 26; i++)
-    {
-        if (alpha[i] >= 5) answer += (char)(i + 'a');
-    }
-
-    if (answer != "") cout << answer << '\n';
-    else cout << "PREDAJA" << '\n';
-
-    return 0;
+	for (int i = 0; i < 26; i++)
+	{
+		if (alpha[i] >= 5) ans += (char)(i + 'a');
+	}
+	if (ans == "") cout << "PREDAJA\n";
+	else cout << ans << '\n';
+	
+	return 0;
 }
