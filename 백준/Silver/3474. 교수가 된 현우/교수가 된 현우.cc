e2455e8de61a4e1f7281defee653808d6f6fec36
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-05-22 15:21:58 +0900

    [Silver III] Title: 교수가 된 현우, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -1,36 +1,34 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+// BOJ - 3474 : 교수가 된 현우
+#include<bits/stdc++.h>
 using namespace std;
+const int INF = 2147000000;
 
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+int t;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	int n;
-	cin >> n;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int res2 = 0, res5 = 0;
 
-	for (int i = 0; i < n; i++)
-	{
-		int a;
-		cin >> a;
-
-		int res2 = 0, res5 = 0;
-
-		for (int j = 2; j <= a; j *= 2)
-		{
-			res2 += a / j;
-		}
-
-		for (int j = 5; j <= a; j *= 5)
-		{
-			res5 += a / j;
-		}
-
-		cout << min(res2, res5) << "\n";
-	}
-	
-	return 0;
+        // 10
+        // 1 2 3 4 5 6 7 8 9 10
+        //   1   1   1   1   1  => 5 (10/2)
+        //       1       1      => 2 (10/4)
+        //               1      => 1 (10/8) => 8
+        for (int i = 2; i <= n; i *= 2)
+        {
+            res2 += n / i;
+        }
+        for (int i = 5; i <= n; i *= 5)
+        {
+            res5 += n / i;
+        }
+        cout << min(res2, res5) << '\n';
+    }
+    return 0;
 }
