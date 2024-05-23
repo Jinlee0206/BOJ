4b5be69ca3f9e4ed7744495c53586a27af8232ff
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-05-23 18:40:40 +0900

    [Silver I] Title: 극장 좌석, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2302. 극장 좌석/극장 좌석.cc
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.cc
@@ -1,20 +1,19 @@
-// BOJ - 2302 : 극장 좌석
+// BOJ - 2302 :	극장 좌석
 #define _CRT_SECURE_NO_WARNINGS
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 
-int n, m, tmp, cnt[41], dp[41];
+int n, m, tmp, dp[54], cnt[54];
 
 int go(int pos)
 {
 	if (pos >= n - 1) return 1;
-	if (cnt[pos]) return go(pos + 1);
+	if (cnt[pos]) return go(pos + 1); // vip 좌석 변경 불가
 	int& res = dp[pos];
-	if (res != -1) return dp[pos];
+	if (res != -1) return res;
 	res = 0;
-	if (!cnt[pos + 1]) res += (go(pos + 2) + go(pos + 1)); // 다음 좌석이 vip 좌석이 아니라면 앞의 두 좌석을 변경
-	else res += go(pos + 1); // 변경하지 않고 idx만 증가
+	if (!cnt[pos + 1]) res += (go(pos + 2) + go(pos + 1));
+	else res += go(pos + 1);
 	return res;
 }
 
@@ -27,7 +26,7 @@ int main()
 	for (int i = 0; i < m; i++)
 	{
 		cin >> tmp;
-		cnt[tmp - 1] = 1; // 바꿀수 없는 vip 좌석 cnt 배열
+		cnt[tmp - 1] = 1;
 	}
 	memset(dp, -1, sizeof(dp));
 	cout << go(0) << '\n';
