1be81ff40d479877da296488d57137f442a3f5bb
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-09-03 18:10:33 +0900

    [Gold IV] Title: 오큰수, Time: 200 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,39 +1,33 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <stack>
-#include <cstring>
-
+// BOJ - 17298 : 오큰수
+#include <bits/stdc++.h>
 using namespace std;
 
-int res[1000004];
-int v[1000004];
-stack<int> s;
+const int INF = 2147000000;
+
+int n, a[1000004], res[1000004];
+
+stack<int> stk;
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
-	int n;
-	cin >> n;
+    cin >> n;
+    memset(res, -1, sizeof(res));
 
-	memset(res, -1, sizeof(res));
-	for (int i = 0; i < n; i++)
-	{
-		cin >> v[i];
-		while (!s.empty() && v[s.top()] < v[i])
-		{
-			res[s.top()] = v[i];
-			s.pop();
-		}
-		s.push(i);
-	}
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        while (stk.size() && a[stk.top()] < a[i])
+        {
+            res[stk.top()] = a[i];
+            stk.pop();
+        }
+        stk.push(i);
+    }
 
-	for (int i = 0; i < n; i++)
-	{
-		cout << res[i] << " ";
-	}
-
-	return 0;
+    for (int i = 0; i < n; i++) cout << res[i] << " ";
+    
+    return 0;
 }
