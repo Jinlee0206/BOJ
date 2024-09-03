95ec9b652c3063c6ab1293b0fe823c2c8910efd5
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-09-03 15:23:47 +0900

    [Silver I] Title: 효율적인 해킹, Time: 2576 ms, Memory: 3540 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -1,17 +1,16 @@
-// BOJ-1325 효율적인 해킹
+// BOJ - 1325 : 효율적인 해킹
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
-vector<int> adj[10001];
-int visited[10001], dp[10001], mx;
+const int INF = 2147000000;
 
-// 해당 지점에 대한 최대 연결 값 구하기
+int n, m, visited[10004], dp[10004], mx = -INF;
+vector<int> adj[10004];
+
 int dfs(int here)
 {
-    visited[here] = 1;
     int res = 1;
-
+    visited[here] = 1;
     for (int there : adj[here])
     {
         if (visited[there]) continue;
@@ -23,9 +22,18 @@ int dfs(int here)
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
+    cin.tie(NULL); cout.tie(NULL);
+
     cin >> n >> m;
+
+    /*
+    1 -> 3
+    2 -> 3
+    3 -> 4, 5
+    4 
+    5 
+    */
+
     for (int i = 0; i < m; i++)
     {
         int a, b;
@@ -33,14 +41,13 @@ int main()
         adj[b].push_back(a);
     }
 
-    // 모든 정점에 대하여 탐색 진행
     for (int i = 1; i <= n; i++)
     {
-        memset(visited, 0, sizeof(visited)); // 방문처리 배열은 초기화
-        dp[i] = dfs(i); // 각 정점에 대한 값 dp배열에 저장
-        mx = max(dp[i], mx); // 최대값 저장
+        memset(visited, 0, sizeof(visited));
+        dp[i] = dfs(i);
+        mx = max(mx, dp[i]);
     }
-    
+
     for (int i = 1; i <= n; i++) if (mx == dp[i]) cout << i << ' ';
 
     return 0;
