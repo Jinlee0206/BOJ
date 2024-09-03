85389794cbf9107a7d4e98127012e54d6068d671
Jin Lee <105345909+Jinlee0206@users.noreply.github.com>
2024-09-03 15:10:26 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.cc
+++ 백준/Gold/1068. 트리/트리.cc
@@ -1,48 +1,44 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+// BOJ - 1068 : 트리
+#include <bits/stdc++.h>
 using namespace std;
 
-int n, k, root;
-vector<int> adj[51];
+int n, root, k;
+vector<int> adj[54];
 
-int dfs(int here)
+int go(int here)
 {
-	int res = 0, child = 0;
+    int res = 0;
+    int child = 0;
 
-	for (int there : adj[here])
-	{
-		if (there == k) continue;
-		res += dfs(there);
-		child++;
-	}
-	// 자식이 없다면 리프노드
-	if (child == 0) return 1;
-	return res;
+    for (int there : adj[here])
+    {
+        if (there == k) continue;
+        res += go(there);
+        child++;
+    }
+    if (child == 0) return 1;
+    else return res;
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n;
+    cin >> n;
 
-	for (int i = 0; i < n; i++)
-	{
-		int tmp;
-		cin >> tmp;
-		if (tmp == -1) root = i;
-		else adj[tmp].push_back(i);
-	}
+    for (int i = 0; i < n; i++)
+    {
+        int tmp;
+        cin >> tmp;
+        if (tmp == -1) root = i;
+        else adj[tmp].push_back(i);
+    }
 
-	cin >> k;
+    cin >> k;
 
-	// DFS가 here -> there, there 기반 코드
-	// root 노드에 대한 반례 필요
-	if (k == root) cout << 0 << "\n";
-	else cout << dfs(root) << "\n";
+    if(k == root) cout << 0 << '\n';
+    else cout << go(root) << '\n';
 
-	return 0;
+    return 0;
 }
